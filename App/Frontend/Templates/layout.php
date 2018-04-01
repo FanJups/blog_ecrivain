5a00daf706cd7d0ef5a90fe70c8ad81030c6fcaa
Fanon JUPKWO <jupsfan@gmail.com>
2018-04-01 22:25:17 +0200

    Modification des appelations

--- App/Frontend/Templates/layout.php
+++ App/Frontend/Templates/layout.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <title>
-      <?= isset($title) ? $title : 'Mon super site' ?>
+      <?= isset($title) ? $title : "Billet simple pour l'Alaska" ?>
     </title>
 
     <script src='https://cloud.tinymce.com/stable/tinymce.min.js'></script>
@@ -14,26 +14,38 @@
   </script>
     
     <meta charset="utf-8" />
+
+    <meta name="description" content="Billet simple pour l'Alaska de Jean Forteroche, acteur et écrivain ">
+
+    <meta name="author" content="Jean Forteroche, acteur et écrivain">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
     
     <link rel="stylesheet" href="/css/Envision.css" type="text/css" />
+
+    <link rel="stylesheet" href="/css/style.css" type="text/css" />
+
+    
+
+    
   </head>
   
-  <body>
+  <body id="blog">
     <div id="wrap">
       <header>
-        <h1><a href="/">Mon super site</a></h1>
-        <p>Comment ça, il n'y a presque rien ?</p>
+        <h1><a href="/">Billet simple pour l'Alaska</a></h1>
+        <p>Par Jean Forteroche, acteur et écrivain</p>
       </header>
       
       <nav>
         <ul>
           <li><a href="/">Accueil</a></li>
-          <li><a href="/admin/">Admin</a></li>
+          <li><a href="/admin/">Gestion des épisodes</a></li>
           <?php if ($user->isAuthenticated()) { 
 
             ?>
           
-          <li><a href="/admin/news-insert.html">Ajouter une news</a></li>
+          <li><a href="/admin/news-insert.html">Ajouter un épisode</a></li>
           <li><a href="/admin/deconnexion">Déconnexion</a></li>
           <?php } ?>
         </ul>
@@ -49,5 +61,7 @@
     
       <footer></footer>
     </div>
+
+
   </body>
 </html>
