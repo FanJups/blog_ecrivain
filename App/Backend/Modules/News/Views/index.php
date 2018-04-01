1fa33a873d1688be5b372c98e18d2fe9dcf0008c
Fanon JUPKWO <jupsfan@gmail.com>
2018-04-01 17:35:32 +0200

    Update index.php

--- App/Backend/Modules/News/Views/index.php
+++ App/Backend/Modules/News/Views/index.php
@@ -1,4 +1,21 @@
-<p style="text-align: center">Il y a actuellement <?= $nombreNews ?> news. En voici la liste :</p>
+
+<?php
+
+	if($nombreNews==0)
+	{
+
+		?>
+			<p style="text-align: center">Aucune news</p>
+
+		<?php
+
+	}
+	else
+	{
+		?>
+
+
+			<p style="text-align: center">Il y a actuellement <?= $nombreNews ?> news. En voici la liste :</p>
 
 <table>
   <tr><th>Auteur</th><th>Titre</th><th>Date d'ajout</th><th>Dernière modification</th><th>Action</th></tr>
@@ -9,3 +26,53 @@ foreach ($listeNews as $news)
 }
 ?>
 </table>
+
+
+
+		<?php
+	}
+
+?>
+
+
+
+
+
+<?php
+
+if($nombreCommentsSignales > 0)
+{
+	?>
+
+		<p style="text-align: center">Il y a actuellement <?= $nombreCommentsSignales ?> commentaire(s) signalé(s). En voici la liste :</p>
+
+<table>
+	
+	<tr><th>News en question</th><th>Auteur</th><th>Contenu</th><th>Action</th></tr>
+
+	<?php
+
+	foreach ($listeCommentsSignales as $commentsSignales)
+	{
+		echo "<tr><td><a href=/news-".$commentsSignales['news'].".html>News</a></td><td>".$commentsSignales['auteur']."</td><td>".$commentsSignales['contenu']."</td><td><a href=/admin/comment-maintenir-".$commentsSignales['id'].".html><img src='/images/update.png' alt='Maintenir' /></a><a href=/admin/comment-delete-".$commentsSignales['id'].".html><img src='/images/delete.png' alt='Supprimer' /></a></td></tr>", "\n" ;
+	} 
+		
+
+
+	?> 
+
+</table>
+
+
+	<?php
+}
+else
+{
+	?>
+		<p style="text-align: center">Aucun commentaire signalé</p>
+	<?php
+
+}
+
+?>
+
